eef178516a954631a7243e85f142c34a55a1793d
Meshack Ogeto <mesharkz1@gmail.com>
2018-10-24 21:10:01 +0300

    [Finishes #161445448] add more Authorization tests to the suites

--- app/tests/v2/test_products.py
+++ app/tests/v2/test_products.py
@@ -17,6 +17,19 @@ class TestProductsFunctions(InitialSetup):
         self.assertEqual(Message,"Product Sucessfully edited")
         self.assertEqual(feedback.status_code,200)
 
+    '''Tests Authorization msg for attendant modify attempts'''
+    def test_returns_no_permision_if_attendant_modifies_a_product(self):
+        feedback = self.app.put(
+            '{}products/1'.format(self.base_url), 
+            headers = dict(Authorization="Bearer " + self.auth_token),
+            data = json.dumps(self.product_details), 
+            content_type='application/json'
+            )
+        Message = json.loads(feedback.data)["Message"]
+        self.assertEqual(Message,"You dont have permissions to modify a product")
+        self.assertEqual(feedback.status_code,200)
+
+
     '''Tests whether the product was successfully deleted'''
     def test_product_successfully_deleted(self):
         feedback = self.app.delete(
@@ -28,3 +41,29 @@ class TestProductsFunctions(InitialSetup):
         Message = json.loads(feedback.data)["Message"]
         self.assertEqual(Message,"Product Sucessfully deleted")
         self.assertEqual(feedback.status_code,200)
+
+    '''Tests whether a message is returned if a product is not available'''
+    def test_return_error_message_if_no_product_to_delete(self):
+        feedback = self.app.delete(
+            '{}products/23'.format(self.base_url), 
+            headers = dict(Authorization="Bearer " + self.auth_token),
+            data = json.dumps(self.product_details), 
+            content_type='application/json'
+            )
+        Message = json.loads(feedback.data)["Message"]
+        self.assertEqual(Message,"Product Not available")
+        self.assertEqual(feedback.status_code,204)
+    
+    '''Tests if an attendant gets error on delete'''
+    def test_return_error_if_no_permission_to_delete(self):
+        feedback = self.app.delete(
+            '{}products/1'.format(self.base_url), 
+            headers = dict(Authorization="Bearer " + self.auth_token),
+            data = json.dumps(self.product_details), 
+            content_type='application/json'
+            )
+        Message = json.loads(feedback.data)["Message"]
+        self.assertEqual(Message,"You dont have permissions to delete this product")
+        self.assertEqual(feedback.status_code,403)
+
+    
--- app/tests/v2/test_user_authentication.py
+++ app/tests/v2/test_user_authentication.py
@@ -4,6 +4,8 @@ from flask import json
 from app.tests.v2.setup_test import InitialSetup
 
 class TestUserRegistration(InitialSetup):
+
+    """Test whether registraton goes successfully"""
     def test_that_a_user_was_succcessfully_registered(self):
         response = self.app.post(
             '{}auth/signup'.format(self.base_url), 
@@ -13,7 +15,19 @@ class TestUserRegistration(InitialSetup):
             )
         Message = json.loads(response.data)["Message"]
         self.assertEqual(Message,"User was created Sucessfully registered")
-        self.assertEqual(response.status_code,201)
+        self.assertEqual(response.status_code,201)    
+        
+    """Test whether it gives authorization message to attendants who want to register"""
+    def test_denies_attendant_access_to_registration(self):
+        response = self.app.post(
+            '{}auth/signup'.format(self.base_url), 
+            headers = dict(Authorization="Bearer " + self.auth_token),
+            data = json.dumps(self.registration_details), 
+            content_type='application/json'
+            )
+        Message = json.loads(response.data)["Message"]
+        self.assertEqual(Message,"You dont have access rights to registration module")
+        self.assertEqual(response.status_code,403)
 
     def test_successfull_user_login(self):
         response = self.app.post(
@@ -25,6 +39,7 @@ class TestUserRegistration(InitialSetup):
         self.assertEqual(Message,"Logged In successfully")
         self.assertEqual(feedback.status_code,200)
     
+    """Gives feedback on unregistered email"""
     def test_gives_error_message_if_email_is_not_registered(self):
         response = self.app.post(
             '{}auth/login'.format(self.base_url), 
@@ -37,6 +52,7 @@ class TestUserRegistration(InitialSetup):
         Message = json.loads(response.data)["Message"]
         self.assertEqual(Message,"shark@mesh.com is not a registered user")
         
+    """Gives feedback on bad password""" 
     def test_gives_error_message_if_wrong_password_entered(self):
         response = self.app.post(
             '{}auth/login'.format(self.base_url), 
