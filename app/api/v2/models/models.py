02201b2d0adf7ef0471454789509b893672c56cb
Meshack Ogeto <mesharkz1@gmail.com>
2018-10-30 12:48:27 +0300

    [#161580805] Create models and views

---
+++ app/api/v2/models/models.py
@@ -0,0 +1,12 @@
+class UserModel:
+    def __init__(self, username, email, password, role):
+        self.username = username
+        self.email = email
+        self.password = password
+        self.role = role
+
+    def creat_user(self):
+        pass
+    
+    def get_user_by_id(self):
+        pass
