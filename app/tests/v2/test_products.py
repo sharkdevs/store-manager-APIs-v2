98824654b9ff659663b545ccfd3e1185df6e92ac
Meshack Ogeto <mesharkz1@gmail.com>
2018-11-01 05:53:56 +0300

    [#161639743] Add tests to the delete to the views and models

--- app/tests/v2/test_products.py
+++ app/tests/v2/test_products.py
@@ -83,17 +83,18 @@ class TestProductsFunctions(InitialSetup):
         self.assertEqual(feedback.status_code,401)
 
 
-    # '''Tests whether the product was successfully deleted'''
-    # def test_product_successfully_deleted(self):
-    #     feedback = self.app.delete(
-    #         '{}products/1'.format(self.base_url),
-    #         headers = dict(Authorization="Bearer " + self.auth_token),
-    #         data = json.dumps(self.product_details),
-    #         content_type='application/json'
-    #         )
-    #     Message = json.loads(feedback.data)["Message"]
-    #     self.assertEqual(Message,"Product Sucessfully deleted")
-    #     self.assertEqual(feedback.status_code,200)
+    '''Tests whether the product was successfully deleted'''
+    def test_product_successfully_deleted(self):
+        auth_token = self.admin_login()
+        self.creat_product()
+        feedback = self.app.delete(
+            '{}products/1'.format(self.base_url),
+            headers = dict(Authorization="Bearer " + auth_token),
+            content_type='application/json'
+            )
+        Message = json.loads(feedback.data)["message"]
+        self.assertEqual(Message,"product successfully deleted")
+        self.assertEqual(feedback.status_code,200)
 
     '''Tests whether a message is returned if a product is not available'''
     def test_return_error_message_if_no_product(self):
@@ -108,14 +109,16 @@ class TestProductsFunctions(InitialSetup):
         self.assertEqual(Message,"That product is not in store")
         self.assertEqual(feedback.status_code,404)
 
-    # '''Tests if an attendant gets error on delete'''
-    # def test_return_error_if_no_permission_to_delete(self):
-    #     feedback = self.app.delete(
-    #         '{}products/1'.format(self.base_url),
-    #         headers = dict(Authorization="Bearer " + self.auth_token),
-    #         data = json.dumps(self.product_details),
-    #         content_type='application/json'
-    #         )
-    #     Message = json.loads(feedback.data)["Message"]
-    #     self.assertEqual(Message,"You dont have permissions to delete this product")
-    #     self.assertEqual(feedback.status_code,403)
+    '''Tests if an attendant gets error on delete'''
+    def test_return_error_if_no_permission_to_delete(self):
+        self.register_attendant()
+        auth_token = self.attendant_login()
+
+        feedback = self.app.delete(
+            '{}products/1'.format(self.base_url),
+            headers = dict(Authorization="Bearer " + auth_token),
+            content_type='application/json'
+            )
+        Message = json.loads(feedback.data)["message"]
+        self.assertEqual(Message,"You dont have permissions to delete a product")
+        self.assertEqual(feedback.status_code,401)
